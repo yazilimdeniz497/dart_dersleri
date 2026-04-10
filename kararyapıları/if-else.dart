222e034db2fb76d1208f7da221dd09551952bd80
denizdogruel00 <denizyazilm@gmail.com>
2026-04-10 17:51:36 +0300

    if-else ve switch case öğrenildi

---
+++ kararyapıları/if-else.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  int sayi1=30;
+  int sayi2=30;
+  // if(sayi1>sayi2){
+  //   print("$sayi2 $sayi1 sayisindan küçüktür ");
+  // }else{
+  //   print("$sayi1 $sayi2 sayisindan küçüktür. ");
+  // }
+  //  if(sayi1>sayi2){
+  //   print("$sayi2 $sayi1 sayisindan küçüktür ");
+  // }else if(sayi1<sayi2){
+  //   print("$sayi1 $sayi2 sayisindan küçüktür. ");
+  // }else{
+  //   print("sayilar eşittir");
+  // }
+}
