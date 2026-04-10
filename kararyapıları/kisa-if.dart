7cdb57e1db8acf4a932bd8ff3ae565a614340f82
denizdogruel00 <denizyazilm@gmail.com>
2026-04-10 17:52:03 +0300

    switch case ileif else ekledim

---
+++ kararyapıları/kisa-if.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  // int sayi1=15;
+  // int sayi2=8;
+  // int kucukSayi=0;
+  // if(sayi1<sayi2){
+  //   kucukSayi=sayi1;
+
+  // }else{
+  //   kucukSayi=sayi2;
+  // }
+  // print("tanimlanan sayi iki sayidan küçük olanı:$kucukSayi");
+  // sayi1 < sayi2 ? kucukSayi=sayi1:kucukSayi=sayi2;
+  // print("tanimlanan sayi iki sayidan küçük olanı:$kucukSayi");
+  // String isim="emre",ad="Emre";
+  // if(isim==ad){
+  //   print("isimler eşit");
+  // }else{
+  //   print("isimler eşit değildir");
+  // }
+}
---
+++ kararyapıları/switch.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  // String notdegeri="AA";
+  // switch(notdegeri){
+  //   case "AA":
+  //   print("AA");
+  //   case "BB":
+  //   print("BB");
+  //   case "CC":
+  //   print("CC");
+  //   default:
+  //   print("DD");
+   int sayi=60;
+   int bolum=sayi~/10;
+   switch(bolum){
+    case 6:
+    print("sayi 60 'tan büyüktür");
+    case 5:
+    print("sayi 50'den büyüktür.");
+    default:
+    print("hata");
+   }
+
+  }
