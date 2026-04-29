d00a85e1cd6b61736061ee05f6c8a5c5f6f67637
denizdogruel00 <denizyazilm@gmail.com>
2026-04-29 15:19:06 +0300

    fİNAL

--- Veritabanı-Dosyalama/mysqlornek/bin/mysqlornek.dart
+++ Veritabanı-Dosyalama/mysqlornek/bin/mysqlornek.dart
@@ -3,7 +3,7 @@ import 'package:mysql1/mysql1.dart';
 
 void main(List<String> arguments) async{
 var settings = ConnectionSettings(
-  host: 'localhost', 
+  host: 'localhost',
   port: 3307,
   user: 'root',
   password: 'toor',
@@ -12,16 +12,19 @@ var settings = ConnectionSettings(
 var conn = await MySqlConnection.connect(settings);
 print("kapandı");
 // ... bağlantı ayarları ve query ...
-var results = await conn.query("select * from kisiselbilgiler");
+var results1 = await conn.query("select * from kisiselbilgiler");
 
-for(var row in results){
+for(var row in results1){
   print("ID: ${row[0]} Name:${row[1]} Surname:${row[2]} City:${row[3]} ");
   // conn.close() buradan kaldırıldı
 }
 
 // Bağlantıyı tüm veriler işlendikten sonra burada kapat
-await conn.close(); 
+
 print("Bağlantı başarıyla kapatıldı.");
+
+// var results=await conn.query("insert into kisiselbilgiler (ad,soyad,sehir) values (?,?,?)",['Ali','Çınar','izmir']);
+await conn.close();
 }
 
 
